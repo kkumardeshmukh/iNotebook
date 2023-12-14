a5e528a0ee90a20ddcea885f4f6a143e1b156ee0
kkumardeshmukh <kdkrantikumar@gmail.com>
2023-12-14 13:01:21 +0530

    navbar home about done . all working fine

--- src/App.js
+++ src/App.js
@@ -1,10 +1,20 @@
 
 import './App.css';
+import About from './components/About';
+import Home from './components/Home';
+import Navbar from './components/Navbar';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 
 function App() {
   return (
-    <div className="App">
-      <h1> this is i notebook</h1>
+    <div>
+      <BrowserRouter>
+      <Navbar title="The iNotebooks"/>
+      <Routes>
+        <Route exact path="/home" element={<Home/>}></Route>
+        <Route exact path="/about" element={<About/>}></Route>
+      </Routes>
+      </BrowserRouter>
     </div>
   );
 }
